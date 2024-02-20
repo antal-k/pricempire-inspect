483f07d34aa0275c873479051f011240e6545922
Antal <antal@pricempire.com>
2024-02-20 21:14:55 +0100

    Update dependencies in package.json and add ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "inspect",
+      exec_mode: "cluster",
+      instances: 4,
+      script: "index.js",
+      combine_logs: true,
+      time: true,
+      node_args: ["--max_old_space_size=2000"],
+    },
+  ],
+};
